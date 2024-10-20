1d33d3c1312a42d91bb9902653e68cd0f0e9c606
Mersad <nickmersad81@gmail.com>
2024-10-20 12:55:50 +0330

    second commit

---
+++ lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:nike_ecommerce_flutter/Core/Constant/Theme/light_theme.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    const defaultTextStyle = TextStyle(
+      color: LightTheme.primaryTextColor,
+      fontFamily: 'IranSanse',
+    );
+    const captionTextStyle = TextStyle(
+      fontFamily: 'IranSanse',
+      color: LightTheme.secondryTextColor,
+      fontSize: 12,
+    );
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Nike',
+      theme: ThemeData(
+        textTheme: const TextTheme(
+          bodySmall: captionTextStyle,
+          labelLarge: defaultTextStyle,
+        ),
+        colorScheme: const ColorScheme.light(
+          onSecondary: LightTheme.primaryColor,
+          primary: LightTheme.primaryColor,
+          secondary: LightTheme.secondryColor,
+        ),
+        useMaterial3: true,
+      ),
+      home: const Directionality(
+          textDirection: TextDirection.rtl,
+          child: MyHomePage(title: 'فروشگاه نایکی')),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Text(
+              'سلام خوش اومدی',
+              style: Theme.of(context).textTheme.bodyLarge,
+            ),
+            Text(
+              'سلامممم خوش اومدی',
+              style: Theme.of(context).textTheme.bodySmall,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
